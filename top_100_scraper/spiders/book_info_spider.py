b1bc1e4605d32cc8f9f77d3f26715f1fe9b3912e
Aaron <aaron.horsager@gmail.com>
2019-08-23 08:20:56 -0400

    changes pages xpath, new try/excepts

--- top_100_scraper/spiders/book_info_spider.py
+++ top_100_scraper/spiders/book_info_spider.py
@@ -17,7 +17,7 @@ class BookInfoSpider(scrapy.Spider):
 
             book_info = BookItem()
 
-            book_info['f_rank'] = int("".join(filter(str.isdigit,
+            book_info['f_rank'] = int(''.join(filter(str.isdigit,
                 book.xpath('./div/span/span/text()').get())))
 
             next_book_url = book.xpath('./span/a/@href').get()
@@ -38,21 +38,21 @@ class BookInfoSpider(scrapy.Spider):
     def parse_book(self, response):
         book_info = response.meta['item']
 
-        book_info['title'] = " ".join(response.xpath(
+        book_info['title'] = ' '.join(response.xpath(
             '//span[@id = "ebooksProductTitle"]/text()').get().split())
 
         book_info['author'] = response.xpath('//div[@id = "bylineInfo"]/span/span[1]'
             '/a/text()').get()
 
-
         book_info['price'] = response.xpath('//tr[@class = "kindle-price"]'
             '//span[@class = "a-size-medium a-color-price"]/text()').get()
 
         group_str = '//div[@id = "reviewFeatureGroup"]'
+
         book_info['series'] = response.xpath(group_str + '/span/a/text()').get()
 
         try:
-            book_info['series_num'] = int("".join(filter(str.isdigit,
+            book_info['series_num'] = int(''.join(filter(str.isdigit,
                 response.xpath(group_str + '/span/b').get())))
         except:
             book_info['series_num'] = None
@@ -65,13 +65,20 @@ class BookInfoSpider(scrapy.Spider):
 
         book_info['rating'] = float(response.xpath(group_str + '//div[@id = "averageCustomerReviews"]'
             '/span/span/@title').get().split()[0])
+
         book_info['review_count'] = int((response.xpath(
             group_str + '//span[@id = "acrCustomerReviewText"]/text()').get().
             replace(',','')).split()[0])
 
         detail_str = '//div[@id = "detail-bullets"]//div[@class = "content"]'
-        book_info['page_count'] = int(response.xpath(
-            detail_str + '/ul/li[2]/text()').get().replace(',','').split()[0])
+
+        try:
+            book_info['page_count'] = int(response.xpath(
+                detail_str + '//li/b[contains(text(), "Print Length:")]/../text()')
+                .get().replace(',','').split()[0])
+        except:
+            book_info['page_count'] = None
+
         try:
             book_info['publisher'] = response.xpath(detail_str +
                 '//li/b[contains(text(), "Publisher:")]/../text()').get().strip()
@@ -80,7 +87,6 @@ class BookInfoSpider(scrapy.Spider):
 
         book_info['all_rank'] = int(((' '.join(response.xpath(detail_str +
             '//li[@id = "SalesRank"]/text()').getall()[1].split())).replace('#','').split())[0])
-
 
         if response.xpath('//span[@id = "upsell-button"]').get() is not None:
             book_info['ku'] = 'yes'
