bf4c652719c3a63e510ec3cd71d14ed79fcc5694
Aaron <aaron.horsager@gmail.com>
2019-08-23 08:35:10 -0400

    fixes User agent syntax

--- top_100_scraper/settings.py
+++ top_100_scraper/settings.py
@@ -16,8 +16,7 @@ NEWSPIDER_MODULE = 'top_100_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4)' +
-    ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
