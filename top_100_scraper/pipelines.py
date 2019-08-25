ce9f596250714b5091a8610bfa1ce98f0f788c20
Aaron <aaron.horsager@gmail.com>
2019-08-25 11:33:35 -0400

    fixes syntax error

--- top_100_scraper/pipelines.py
+++ top_100_scraper/pipelines.py
@@ -46,7 +46,8 @@ class Top100ScraperPipeline(object):
             print(book_info['review_count'])
 
         try:
-            book_info['page_count'] = int([book_info['page_count'].replace(',','').split()[0])
+            book_info['page_count'] = int(([book_info['page_count'].
+                replace(',','')).split()[0])
         except:
             print('No page count for ' + book_info['title'])
             print(book_info['page_count'])
