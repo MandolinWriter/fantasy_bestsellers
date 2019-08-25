6770d37d73a20aea6c4ff561f367dab04e752af1
Aaron <aaron.horsager@gmail.com>
2019-08-25 11:40:47 -0400

    updates user agetn

--- top_100_scraper/settings.py
+++ top_100_scraper/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'top_100_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
